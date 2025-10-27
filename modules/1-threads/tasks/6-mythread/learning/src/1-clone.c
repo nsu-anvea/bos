9a61068eef278f7724bb82e44786d7a35d5a87f3
anveretal <anveretal@mail.ru>
2025-10-27 15:33:48 +0700

    added 1-clone.c

---
+++ modules/1-threads/tasks/6-mythread/learning/src/1-clone.c
@@ -0,0 +1,40 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sched.h>
+
+int thread_function(void *arg) {
+    printf("Привет из потока! Аргумент: %s\n", (char *) arg);
+    return 42;
+}
+
+int main() {
+    void *stack = malloc(1024 * 1024);
+    if (!stack) {
+        perror("malloc");
+        return 1;
+    }
+
+    void *stack_top = stack + 1024 * 1024;
+
+    int tid = clone(
+        thread_function,
+        stack_top,
+        CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD,
+        "test"
+    );
+
+    if (tid == -1) {
+        perror("clone");
+        free(stack);
+        return 1;
+    }
+    
+    printf("Поток создан с tid: %d\n", tid);
+    sleep(1);
+    free(stack);
+
+    return 0;
+}
