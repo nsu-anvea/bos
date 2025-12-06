0cc7e1f0ecd08d9fd8ac0f2321dfc137077fd769
anveretal <anveretal@mail.ru>
2025-12-07 00:26:36 +0700

    added 1-spinlock to task 2-3

---
+++ modules/2-synchronization/tasks/3-concurrent-linked-list/1-spinlock/storage.h
@@ -0,0 +1,25 @@
+#ifndef __STORAGE_H__
+#define __STORAGE_H__
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+typedef struct _Node {
+    char value[100];
+    struct _Node *next;
+    pthread_spinlock_t sync;
+} Node;
+
+typedef struct _Storage {
+    Node *first;
+    int length;
+} Storage;
+
+Storage *storage_init(int size);
+void storage_destroy(Storage *storage);
+
+#endif // __STORAGE_H__
